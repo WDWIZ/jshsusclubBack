f390408b1742c6d4189a60d23c93c08ad5fd0a46
WDWIZ <140268841+WDWIZ@users.noreply.github.com>
2024-03-01 02:59:57 +0900

    jshsusClubBack
    jshsusClubBack

---
+++ api/sockethandler.js
@@ -0,0 +1,36 @@
+const socketHandler = (io, db) => {
+    io.on('connection', (socket) => {
+        let userID;
+         
+        socket.on("ping", (data) => {
+            socket.emit({msg: "pong"});
+        });
+    
+        socket.on("login", async (data) => {
+            userID = data.userID;
+        });
+    
+        socket.on('myClubs', async () => {
+            if (!userID) return;
+    
+            let arr;
+            const lists = await db.clubs.findAll({
+                attributes: ['name', 'id'],
+                where: {
+                    leader: userID
+                }
+            });
+    
+            arr = Array.from(Array(lists.length), () => Array(0).fill(""));
+    
+            arr = arr.map((x, idx) => {
+                x = {id: lists[idx].id, name: lists[idx].name};
+                return x;
+            });
+    
+            socket.emit('yourClubs', arr);
+        });
+    });
+}
+
+module.exports = socketHandler;
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const db = require('./models');
+
+require('dotenv').config();
+
+express.urlencoded({ extended : false });
+
+app.use(express.json());
+app.use(cors());
+
+const clubRouter = require('./routes/clubs.js')();
+app.use("/clubs", clubRouter);
+
+app.get('/userInfo', async (req, res) => {
+    const userID = req.query.userID;
+
+    const results = await db.users.findAll({
+        attributes: ['stuid', 'name'],
+        where: {
+            id: userID
+        }
+    });
+
+    res.json({name: results[0].name, stuid: results[0].stuid});
+});
+
+module.exports = app;
