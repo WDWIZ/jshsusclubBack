e02e220d1b2f2ec13912c3c673a58738e02c1699
WDWIZ <140268841+WDWIZ@users.noreply.github.com>
2024-03-27 11:38:23 +0900

    connection

--- config/index.js
+++ config/index.js
@@ -25,7 +25,16 @@ const production = {
     "database": "plma",
     "host": "points.jshsus.kr",
     "dialect": "mysql",
-    "logging": false
+    "logging": false,
+    "dialectOptions": {
+      "connectTimeout": 60000 // Increase the connection timeout (in milliseconds)
+    },
+    "pool": {
+      "max": 5, // Maximum number of connection in pool
+      "min": 0, // Minimum number of connection in pool
+      "acquire": 30000, // The maximum time, in milliseconds, that pool will try to get connection before throwing error
+      "idle": 10000 // The maximum time, in milliseconds, that a connection can be idle before being released
+    }
 }
 
 
